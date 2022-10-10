2fd709525fea044281c854b67debb7e417e4110f
ChingisBadmaev <chingis.badmaev2018@yandex.ru>
2022-10-10 21:35:02 +0300

    add hw_3

---
+++ python_course/hw_3/fetch_values.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def file_lines_iterator(filename):
+    with open(filename, 'r') as f:
+        for row in f:
+            yield row
+
+
+def process_row(source):
+    x_data = []
+    y_data = []
+    z_data = []
+    for row in source:
+        for item in row.split(','):
+            elem = item.strip(' = ')
+            if elem[0] == 'x':
+                x_data.append(elem[3:])
+            if elem[0] == 'y':
+                y_data.append(elem[3:])
+            if elem[0] == 'z':
+                z_data.append(elem[3:][0:-1])  # [0:-1] чтобы не было '\n'
+    return x_data, y_data, z_data
+
+
+print('enter the filename:')
+filename = input()
+df = pd.DataFrame(process_row(file_lines_iterator(filename))).T
+
+df.rename({0: 'x', 1: 'y', 2: 'z'}, axis=1, inplace=True)
+df['x'] = pd.to_numeric(df['x'])
+df['z'] = pd.to_numeric(df['z'])
+df.to_csv(f'after_change_{filename}', index=False)
